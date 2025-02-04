a4819194f8a28482426f4b133c8fd3a2e84ce686
CelestiaLKamI <work.mayukh14@gmail.com>
2025-02-04 19:33:53 +0530

    Refactor menu.py: rename "Mayukh Scripts Test" to "Mayukh Scripts" for clarity

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@ from light_editor_tool import light_editor
 print("Loading menu.py...")
 
 menu = nuke.menu("Nuke")
-my_scripts = menu.addMenu("Mayukh Scripts Test")
+my_scripts = menu.addMenu("Mayukh Scripts")
 
 light_editor_tool = my_scripts.addMenu("Light Editor")
 light_editor_tool.addCommand("Light Editor", light_editor)
